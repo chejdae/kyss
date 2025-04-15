402f29c6b2c5ca733dd53d0cf395b297decfe86c
chejdae <jongdae.lee@datable.co.kr>
2025-04-16 00:15:19 +0900

    Update get-ilju.js

--- pages/api/get-ilju.js
+++ pages/api/get-ilju.js
@@ -1,6 +1,6 @@
 import { iljuAnimalDB } from '../../lib/iljuAnimalDB';
 
-const gan = ['갑', '을', ' '병', '정', '무', '기', '경', '신', '임', '계'];
+const gan = ['갑', '을', '병', '정', '무', '기', '경', '신', '임', '계'];
 const ji = ['자', '축', '인', '묘', '진', '사', '오', '미', '신', '유', '술', '해'];
 const ganjiList = Array.from({ length: 60 }, (_, i) => gan[i % 10] + ji[i % 12]);
 const baseDate = new Date('1984-01-01');
